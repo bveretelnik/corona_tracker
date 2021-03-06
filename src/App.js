9dff1d528216e390a8b76f395b8d19cf4e53b49b
Bogdan <bveretelnik@gmail.com>
2021-03-06 11:45:08 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Cards, CountryPicker, Chart } from "./components/indexExport";
 import { fetchData } from "../src/api/index";
+import styles from "./App.module.css";
+import image from "./image/image.png";
+
 export default function App() {
+  const initiaState = {
+    data: {},
+  };
+  const [state, setState] = useState(initiaState);
   useEffect(() => {
     fetchData().then((res) => {
-      const response = res.data;
+      const response = res;
       console.log(response);
+      setState({ data: response });
     });
   }, []);
 
+  const { data } = state;
   return (
-    <div>
-      <Cards />
+    <div className={styles.container}>
+      <img className={styles.image} src={image} alt="COVID-19" />
+      <Cards data={data} />
       <CountryPicker />
-      <Chart />
+      <Chart data={data} />
     </div>
   );
 }
